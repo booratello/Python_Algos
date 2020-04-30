62cfffc3cf458c04fbd07b74f433239a4b64fd98
booratello <booratello@mail.ru>
2020-05-01 03:00:17 +0600

    Домашнее задание к уроку 8 "Деревья. Хэш-функция".

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,23 @@
 
 Итог: 6 подстрок
 """
+
+import hashlib
+from pprint import pprint
+
+S = input("Введите строку из маленьких латинских букв:\n")
+COMB_SET = set()
+COMB_SET_HASH = set()
+
+for i in range(len(S)):
+    for j in range(len(S) - i):
+        SUB_S = S[j:(len(S) - i)]
+        if len(SUB_S) != len(S):
+            COMB_SET.add(SUB_S)
+            COMB_SET_HASH.add(hashlib.sha1(SUB_S.encode('utf-8')).hexdigest())
+
+print(f"Количество уникальных подстрок: {len(COMB_SET)}")
+print(f"Уникальные подстроки:")
+pprint(sorted(list(COMB_SET)))
+print("Хэши уникальных подстрок (порядок не соответствует списку выше):")
+pprint(COMB_SET_HASH)
